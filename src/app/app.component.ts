0026746760851b2078c4b95311cf37b181419f06
paulobdelacruz16 <paulobdelacruz16@gmail.com>
2023-03-18 16:58:40 +0800

    added banner and navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'goal-church-app';
+  constructor() {}
+  navlist: any
+  selectedNavId: any
+
+  ngOnInit(): void {
+     this.navlist = [
+      { name: 'Home', src: "home" },
+      { name: 'Sermons',  src: "sermons" },
+      { name: 'News', src: "news" },
+      { name: 'Events', src: "events" },
+      { name: 'About', src: "about" },
+      { name: 'Contact', src: "contact" },
+      { name: 'Donate', src: "donate" }
+    ];
+    this.selectedNavId = 0;
+  }
+
+  clickNavigation(value: number){
+    this.selectedNavId = value;
+  }
 }
