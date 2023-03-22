ee7a4c292240d9441c696d8d77e227638ee19aee
paulobdelacruz16 <paulobdelacruz16@gmail.com>
2023-03-22 22:59:44 +0800

    change directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { DonateComponent } from './donate/donate.component';
-import { EventsComponent } from './events/events.component';
-import { HomeComponent } from './home/home.component';
-import { NewsComponent } from './news/news.component';
-import { SermonsComponent } from './sermons/sermons.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DonateComponent } from './components/donate/donate.component';
+import { EventsComponent } from './components/events/events.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { SermonsComponent } from './components/sermons/sermons.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from '../app/home/home.component'
-import { NewsComponent } from './news/news.component';
-import { ContactComponent } from './contact/contact.component';
-import { DonateComponent } from './donate/donate.component';
-import { SermonsComponent } from './sermons/sermons.component';
+import { HomeComponent } from '../app/components/home/home.component'
+import { NewsComponent } from './components/news/news.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DonateComponent } from './components/donate/donate.component';
+import { SermonsComponent } from './components/sermons/sermons.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
