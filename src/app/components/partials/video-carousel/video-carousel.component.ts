494cbb2229311665adab2af692db18914a342de8
paulobdelacruz16 <paulobdelacruz16@gmail.com>
2023-03-25 03:00:21 +0800

    added modal

--- src/app/components/partials/video-carousel/video-carousel.component.ts
+++ src/app/components/partials/video-carousel/video-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component,TemplateRef, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component,TemplateRef, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
@@ -13,29 +13,8 @@ export class VideoCarouselComponent implements OnInit {
   constructor(private modalService: BsModalService) {}
   myInterval = 150000000;
 
-  itemListData1:any;
-  itemListData2:any;
-  videoListData:any;
-  itemListData3:any;
-  itemListData4:any;
+  @Input() itemListData:any;
   ngOnInit(): void {
-    this.itemListData4 =  [
-      {
-        description:"Whoever believes and is baptized will be saved",
-        image:"https://drive.google.com/uc?id=1m-5i93zen40JhfLSNem8AFf4XwSdBO1F",
-        url: "#"
-      },
-      {
-        description:"Go and Make Disciples",
-        image:"https://drive.google.com/uc?id=1dbkcVRxS6C-phSVmaDMqS4WhKjchx6v-",
-        url: "#"
-      },
-      {
-        description:"Train up a child in the way he should go.",
-        image:"https://drive.google.com/uc?id=1tJBUg2aG2gMpvOahWH054fOw7OgQ9WgV",
-        url: "#"
-      },
-    ];
   }
 
   openModal(template: TemplateRef<any>) {
