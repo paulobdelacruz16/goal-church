26407e6241b7fd3a7917d4946486098a27fc1d3a
Keane Leigh Merin <kleigh.merin@outlook.com>
2023-03-31 21:43:14 +0800

    Test Data

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -32,8 +32,9 @@ export class HomeComponent implements OnInit {
   videoListData: any;
   itemListData3: any;
   itemListData4: any;
+  testData: any;
   ngOnInit(): void {
-    console.log('homepage');
+    console.log('homepage test');
     this.itemListData1 = {
       title: 'Services',
       description: 'Come, let us worship and bow down, Let us kneel before the Lord our Maker',
@@ -132,7 +133,38 @@ export class HomeComponent implements OnInit {
         url: 'https://www.youtube.com/embed/gDT1ZR9ejzM',
       },
     ]
+   
   };
+
+  this.testData = {
+    title: 'Sermons',
+    description: 'My sheep hear my voice, and I know them, and they follow me. I give them eternal life, and they will never perish, and no one will snatch them out of my hand.',
+    data: [
+    {
+      name: 'Victory Over the Battles',
+      pastor: 'Sis. Malou Barles',
+      image:
+        'https://drive.google.com/uc?id=1zGMw_SK67c2Op1jFSWOyBZ66mnBrfBMS',
+      url: 'https://www.youtube.com/embed/4oW0S9MO0yE',
+    },
+    {
+      name: "God's Love for the One and Expectations from the 99",
+      pastor: 'Pstr. Jay Dela Cruz',
+      image:
+        'https://drive.google.com/uc?id=1qY1MY3Lmu2pdGuWjWnCWozuwp7NNAHdA',
+      url: 'https://www.youtube.com/embed/AKajaSXlOxg',
+    },
+    {
+      name: 'He Knows My Name',
+      pastor: 'Pstr. Jay Dela Cruz',
+      image:
+        'https://drive.google.com/uc?id=1ygK4AjN_A8fvZ48MR_VRc5xw8ZQ5J4ZV',
+      url: 'https://www.youtube.com/embed/gDT1ZR9ejzM',
+    },
+  ]
+};
+
+  
   }
 
   openModal(template: TemplateRef<any>) {
